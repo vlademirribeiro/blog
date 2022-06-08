d857577fb7ebbec021f97a1f49c76f767123f4d1
vlademir <vlademirmatias1@gmail.com>
2022-06-08 16:19:36 -0300

    ajustes

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,5 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_08_162951) do
-  create_table "products", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
+ActiveRecord::Schema[7.0].define(version: 0) do
 end
